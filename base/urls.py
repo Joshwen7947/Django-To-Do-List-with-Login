ab097870ef0a9f04dc8d018e8ed3f3b18d8385ed
Josh <joshuawen97@gmail.com>
2022-11-11 11:21:12 +0700

    backend done - ready for styling

--- base/urls.py
+++ base/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
     path("login/", CustomLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(next_page='login'), name='logout'),
+    path("register/", RegisterPage.as_view(), name='register'),
     path("",TaskList.as_view(), name="tasks"),
     path("task/<int:pk>", TaskDetail.as_view(),name="task" ),
     path("create-task",TaskCreate.as_view(), name="task-create"),
